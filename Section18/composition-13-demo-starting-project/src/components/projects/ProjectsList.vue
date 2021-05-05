859082cd3f25febbdb97f084f4a3e597255e31fc
flyiva-05 <m_akdogan@anadolu.edu.tr>
2021-05-05 16:58:21 +0300

    migration to composition api

---
+++ Section18/composition-13-demo-starting-project/src/components/projects/ProjectsList.vue
@@ -0,0 +1,75 @@
+<template>
+  <base-container v-if="user">
+    <h2>{{ user.fullName }}: Projects</h2>
+    <base-search v-if="hasProjects" @search="updateSearch" :search-term="enteredSearchTerm"></base-search>
+    <ul v-if="hasProjects">
+      <project-item v-for="prj in availableProjects" :key="prj.id" :title="prj.title"></project-item>
+    </ul>
+    <h3 v-else>No projects found.</h3>
+  </base-container>
+  <base-container v-else>
+    <h3>No user selected.</h3>
+  </base-container>
+</template>
+
+<script>
+import { ref, computed, watch } from 'vue';
+import ProjectItem from './ProjectItem.vue';
+
+export default {
+  components: {
+    ProjectItem,
+  },
+  props: ['user'],
+  setup(props) {
+    const enteredSearchTerm = ref('');
+    const activeSearchTerm = ref('');
+
+    const hasProjects = computed(function () {
+      return props.user.projects && availableProjects.value.length > 0;
+    });
+
+    const availableProjects = computed(function () {
+      if (activeSearchTerm.value) {
+        return props.user.projects.filter((prj) =>
+          prj.title.includes(activeSearchTerm.value)
+        );
+      }
+      return props.user.projects;
+    });
+
+    function updateSearch(val) {
+      enteredSearchTerm.value = val;
+
+    }
+
+    watch(enteredSearchTerm, function(newValue) {
+      setTimeout(() => {
+        if (newValue === enteredSearchTerm.value) {
+          activeSearchTerm.value = newValue;
+        }
+      }, 300);
+    });
+
+    watch(props.user, function() {
+      enteredSearchTerm.value = '';
+    });
+
+    return {
+      enteredSearchTerm,
+      activeSearchTerm,
+      hasProjects,
+      availableProjects,
+      updateSearch
+    }
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
